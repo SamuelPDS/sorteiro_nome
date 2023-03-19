cbe325d339c690f4169303bab2f2a870fe048e61
Samuel Charles <112909689+SamuelPDS@users.noreply.github.com>
2023-03-19 17:58:22 -0300

    Create sorteiro.py

---
+++ sorteiro.py
@@ -0,0 +1,8 @@
+import random
+n1 = input("Qual o nome do primeiro aluno?")
+n2 = input("Qual o nome do segundo aluno?")
+n3 = input("Qual o nome do terceiro aluno?")
+n4 = input("Qual o nome do quarto aluno?")
+lista = [n1,n2,n3,n4]
+sorteio = random.choice(lista)
+print("O nome escolhido foi {}".format(sorteio))
